ffafa5052ab28eab38e87fcc2f962cc05ed011a2
ChrisShocker <fallenchris@live.com>
2022-06-13 12:19:46 -0700

    Add non-generic example

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,14 +4,21 @@ namespace Generics
 {
     internal class Program
     {
+
         //non-Generic list
         public class ObjectList
         {
+
+            //list to store object
+            object[] objectArray = new object[1];
+
             //Can add anything as an object but suffers in performance due to 
             //having to box all value types such as an int.
             public void Add(object value)
             {
-                throw new NotImplementedException();
+                Console.WriteLine(value);
+                objectArray[0] = value;
+                Console.WriteLine("object added");
             }
 
             //indexer that returns the object at the given index
@@ -21,9 +28,45 @@ namespace Generics
             }
         }
 
+
+        /*
+         * Generic List
+         * - Parameters specified with < >
+         * - Typically parameters are named T ie <T> where T is for Template or Type
+         */
+
+        public class GenericList<T>
+        {
+            public void Add(T value)
+            {
+
+            }
+            public T this[int index]
+            {
+                set { throw new NotImplementedException(); }
+            }
+
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Non-Generic Example
+
+            //create list
+            var ObjectList = new ObjectList();
+
+            //create an object to add to list
+            var person = new { name = "joe", Age = 21 };
+
+            //add created object to list
+            ObjectList.Add(person);
+
+
+            //Generic Example
+
+
         }
     }
 }
