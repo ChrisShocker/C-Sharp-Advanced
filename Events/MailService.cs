851994af1a9743da320ac6e53f2198d325c31dd0
ChrisShocker <fallenchris@live.com>
2022-06-13 16:23:06 -0700

    Refactor Move Event handler to new file

---
+++ Events/MailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Events
+{
+    internal partial class Program
+    {
+        //4.Create Event handler
+        public class MailService
+        {
+            public void OnVideoEncoded(object source, EventArgs e)
+            {
+                Console.WriteLine("MailService: Sending an email...");
+            }
+        }
+    }
+}
--- Events/Program.cs
+++ Events/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Events
+﻿namespace Events
 {
     internal partial class Program
     {
@@ -46,15 +44,6 @@ namespace Events
 
             //call encode
             videoEncoder.Encode(video);
-        }
-
-        //4.Create Event handler
-        public class MailService
-        {
-            public void OnVideoEncoded(object source, EventArgs e)
-            {
-                Console.WriteLine("MailService: Sending an email...");
-            }
         }
     }
 }
