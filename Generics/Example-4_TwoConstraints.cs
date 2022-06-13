25296772d546e261349dedcdaaeb68fd6ea5d896
ChrisShocker <fallenchris@live.com>
2022-06-13 13:39:19 -0700

    Add generic example with two contraints

---
+++ Generics/Example-4_TwoConstraints.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Generics
+{
+    internal partial class Program
+    {
+            //Example 4: Two Constraints,
+            //Two constraints can be applied to generic classes
+        public class TwoConstraints<T> where T : IComparable, new()
+        {
+            //If we need to instantiate an instance of T
+            //
+            public void DoSomethingWithObject(T value)
+            {
+                //need a default constructor so apply another constraint for the default contstructor ie, new() as second constraint type
+                var obj = new T();
+            }
+        }
+    }
+}
