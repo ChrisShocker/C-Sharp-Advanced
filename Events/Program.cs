ca126175898e6d11ba9b7117cfc237d17efd3a59
ChrisShocker <fallenchris@live.com>
2022-06-13 16:21:20 -0700

    Finish Example

--- Events/Program.cs
+++ Events/Program.cs
@@ -1,13 +1,60 @@
-﻿namespace Events
+﻿using System;
+
+namespace Events
 {
     internal partial class Program
     {
+
+        /*
+         * Events allow objects to trigger one another, 
+         * and allow applications to be more loosely coupled
+         * 
+         * Typical Event Flow:
+         * 1. Define a delgate
+         *   - Takes two parameters: object that's the source of the event, and any args that have information from the event 
+         *   
+         * 2. Define an event
+         *   - Note tense is important
+         * 
+         * 3. Raise the event
+         *  - Need a method to handle raising the event
+         *    - Method starts with "On"
+         *    - Method must be virtual
+         *    - Method must be void
+         * 
+         * 4. Create event handler
+         *  - Create Subscription
+         * 
+         * https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/event
+         */
+
         static void Main(string[] args)
         {
+            //create a video and give it a title
             var video = new Video() { Title = "Video 1" };
+
+            //instantiate VideoEncoder
+            //Publisher
             var videoEncoder = new VideoEncoder();
 
+            //instantiate MailService
+            //Subscriber
+            var mailService = new MailService();
+
+            //4. Create Subscription using the delegate
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+
+            //call encode
             videoEncoder.Encode(video);
+        }
+
+        //4.Create Event handler
+        public class MailService
+        {
+            public void OnVideoEncoded(object source, EventArgs e)
+            {
+                Console.WriteLine("MailService: Sending an email...");
+            }
         }
     }
 }
--- Events/VideoEncoder.cs
+++ Events/VideoEncoder.cs
@@ -7,10 +7,52 @@ namespace Events
     {
         public class VideoEncoder
         {
+            /*
+             * 1. Define a delgate
+             * 2. Define an event
+             * 3. Raise the event
+             */
+
+            /*
+             * 1. Define delegate
+             * - Takes two parameters: object that's the source of the event, and any args that have information from the event 
+             */
+            public delegate void VideoEncodedEventHandler(object source, EventArgs eventArgs);
+
+            /*
+             * 2. Define an event
+             * - Note past tense
+             */
+            public event VideoEncodedEventHandler VideoEncoded; 
+
+            /*
+             * 3. Raise the Event
+             * - Need a method to handle raising the event
+             *      - Start with "On"
+             *      - Method must be virtual
+             *      - Method must be void
+             */
+
+                
+
             public void Encode(Video video)
             {
                 Console.WriteLine("Encoding video...");
                 Thread.Sleep(3000);
+
+                OnVideoEncoded();
+            }
+
+            protected virtual void OnVideoEncoded()
+            {
+                //Do something after the event is done
+                if(VideoEncoded != null)
+                {
+                    //1st arg is the source of the event or class publishing event
+                    //2nd arg is data being sent with the event (none in this case)
+                    VideoEncoded(this, EventArgs.Empty);
+                }
+
             }
         }
     }
