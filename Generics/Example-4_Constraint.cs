b3688f051da8a31a94b693b5927aaaa56532c176
ChrisShocker <fallenchris@live.com>
2022-06-13 13:00:10 -0700

    Add constraint example

---
+++ Generics/Example-4_Constraint.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Generics
+{
+    internal partial class Program
+    {
+        //Example 4:
+        //somtimes we may need to restrain input types
+
+        public class Utilities
+        {
+
+          
+            //Example 4a:
+            //Apply a constraint for typical behavior
+            public int Max(int a, int b)
+            {
+                return a > b ? a : b;
+            }
+
+            //Example 4b:
+            //Generic type won't work, since the input type is unknown,
+            //and comparisons don't work with all types
+            //we must create a generic function and use IComparable to make sure inputs are comparable
+            public T Max<T>(T a, T b) where T : IComparable
+            {
+                return a.CompareTo(b) > 0 ? a : b;
+            }
+
+            /*
+             *Note: We can also move the IComparable to class i.e.,
+             *'public class Utilities where T : IComparable'
+             *
+             *and the function would no longer need it i.e.,
+             *public T Max(T a, T b){
+                return a.CompareTo(b) > 0 ? a : b;
+              }
+             * 
+             */
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,60 +1,12 @@
-﻿using System;
-
-namespace Generics
+﻿namespace Generics
 {
-    internal class Program
+    internal partial class Program
     {
-
-        //Link to Generics Docs
-        //https://docs.microsoft.com/en-us/dotnet/csharp/fundamentals/types/generics
-
-        //non-Generic list
-        public class ObjectList
-        {
-
-            //list to store object
-            object[] objectArray = new object[1];
-
-            //Can add anything as an object but suffers in performance due to 
-            //having to box and unbox all value types such as an int.
-            public void Add(object value)
-            {
-                Console.WriteLine(value);
-                objectArray[0] = value;
-                Console.WriteLine("object added");
-            }
-
-            //indexer that returns the object at the given index
-            public object this[int index]
-            {
-                get { throw new NotImplementedException(); }
-            }
-        }
-
-
-        /*
-         * Generic List
-         * - Parameters specified with < >
-         * - Typically parameters are named T ie <T> where T is for Template or Type
-         */
-
-        public class GenericList<T>
-        {
-            public void Add(T value)
-            {
-
-            }
-            public T this[int index]
-            {
-                set { throw new NotImplementedException(); }
-            }
-
-        }
-
-
 
         static void Main(string[] args)
         {
+            
+            //Example 1:
             //Non-Generic Example
 
             //create list
@@ -67,6 +19,7 @@ namespace Generics
             ObjectList.Add(person);
 
 
+            //Example 2:
             //Generic Example
 
             //create the generic list specifying the type
@@ -78,6 +31,16 @@ namespace Generics
             var list = new GenericList<object>();
             //add an object
             list.Add(new object());
+
+
+            //Example 3:
+
+            //Generic Dictionary Example
+            var dictionary = new GenericDictionary<string, object>();
+            dictionary.Add("123", new object());
+
+
+            //Example 4:
 
         }
     }
