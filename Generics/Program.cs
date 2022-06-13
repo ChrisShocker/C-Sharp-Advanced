cc57f645b31f36facee1526a592a7b64e813bd6f
ChrisShocker <fallenchris@live.com>
2022-06-13 11:56:31 -0700

    6. Generics Create Basic non-generic example

---
+++ Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        //non-Generic list
+        public class ObjectList
+        {
+            //Can add anything as an object but suffers in performance due to 
+            //having to box values such as an int or string.
+            public void Add(object value)
+            {
+                throw new NotImplementedException();
+            }
+
+            //indexer that returns the number at the given index
+            public object this[int index]
+            {
+                get { throw new NotImplementedException(); }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
