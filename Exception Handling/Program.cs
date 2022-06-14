e598f0da9ffa51054e3e2aebc4b4cd883e8eb8a0
ChrisShocker <fallenchris@live.com>
2022-06-14 15:26:41 -0700

    Create example skeleton

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exception_Handling
+{
+    /*
+     * Exception Handling
+     * - Catch exceptions durring runtime to prevent crashing
+     * - Defensive programming technique to provide more robust applications 
+     *
+     * https://docs.microsoft.com/en-us/dotnet/csharp/fundamentals/exceptions/exception-handling
+     */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
