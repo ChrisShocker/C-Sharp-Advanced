387480ef61aea2e5d0b56b1fe454f9dd1d659f8f
ChrisShocker <fallenchris@live.com>
2022-06-14 15:09:21 -0700

    Add example skeleton

---
+++ Dynamic Binding/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dynamic_Binding
+{
+    /*
+     *  Dynamic Binding
+     *  - Allows binding at runtime instead of compile time
+     *  - Skips compile time checks
+     *  
+     *  https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/types/using-type-dynamic
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
