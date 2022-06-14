2d4064e35ee8d43b2c190c282e5c55490f037df8
ChrisShocker <fallenchris@live.com>
2022-06-14 15:50:06 -0700

    Add exception handling example

--- Exception Handling/Program.cs
+++ Exception Handling/Program.cs
@@ -14,6 +14,42 @@ namespace Exception_Handling
     {
         static void Main(string[] args)
         {
+            var calculator = new Calculator();
+            var sum = 1;
+
+
+            try
+            {
+                //will throw an exception since we can't divide by 0
+                sum = calculator.Divide(2, 0);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Can't Divide by 0");
+                Console.WriteLine(e.ToString());
+
+            }
+            catch(Exception otherExceptions)
+            {
+                Console.WriteLine("\n"+otherExceptions.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("\nError Caught");
+                Console.WriteLine("\nDo something with error...");
+            }
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x += y;
+        }
+        public int Divide(int x, int y)
+        {
+            return x / y;
         }
     }
 }
