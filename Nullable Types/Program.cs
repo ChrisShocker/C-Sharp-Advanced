5ecc997fca4f28e95e2529aa9cad464d411a3d97
ChrisShocker <fallenchris@live.com>
2022-06-14 14:38:04 -0700

    Add Nullable example

--- Nullable Types/Program.cs
+++ Nullable Types/Program.cs
@@ -2,11 +2,41 @@
 
 namespace Nullable_Types
 {
+    /*
+     * Nullable Types
+     * - C# value type can't be NULL,
+     *   but in some cases a value should be NULL ie, secondary phone number
+     */
     internal class Program
     {
         static void Main(string[] args)
         {
 
+            /*
+             * Value types can't be set to null
+             * ie 'int secondaryPhone = null' will throw an exception
+             * - We can use the Nullabe generic
+             */
+
+            //Use Nullable generic longhand
+            Nullable<int> secondaryPhone = null;
+
+            secondaryPhone = 1;
+
+            if (secondaryPhone.HasValue)
+                Console.WriteLine("Secondary Phone number: " +secondaryPhone.Value);
+
+            //or shorthand
+            int? secondaryPhone2 = null;
+
+            if (!secondaryPhone2.HasValue)
+            {
+                Console.WriteLine("secondaryPhone2 is null");
+
+            }
+
+
+            
         }
     }
 }
