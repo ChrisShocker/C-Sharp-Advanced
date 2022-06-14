f9c5f0a315d3e30e6775df793e0343600f269440
ChrisShocker <fallenchris@live.com>
2022-06-14 16:04:37 -0700

    Add Memory Managment example Unmanaged resources must be closed manually

---
+++ Memory Management/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Memory_Management
+{
+    /*
+     * Memory Management 
+     * - Unmanaged Resources must be closed manually, 
+     *   since they're not cleared with the default garbage collector.
+     *      - Files, Database connections, Network Connections, Graphic Handles, etc must be closed manually
+     *  
+     *  - Unamanged resources are closed with Dispose or their default disposal method ie, Close()
+     *  
+     * https://docs.microsoft.com/en-us/dotnet/standard/garbage-collection/unmanaged
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = null;
+            try
+            {
+                //open unmanaged resource
+                streamReader = new StreamReader("path");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An exception is caught");
+            }
+            finally
+            {
+                /*
+                 * If the streamReader was created, dispose of it
+                 * 
+                 * Note: .Close can also be used on streamReader, 
+                 * it calls Dispose automatically.
+                 */
+                if(streamReader != null)
+                    streamReader.Dispose();
+                Console.WriteLine("\nResource closed");
+            }
+        }
+    }
+}
